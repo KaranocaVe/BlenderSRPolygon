1e529e1b22149a508d7ede96fb7dc76b81ef9f3e
KaranocaVe <39995482+KaranocaVe@users.noreply.github.com>
2024-05-26 22:59:52 +0800

    Update srPolygonTransform.py

--- src/srPolygonTransform.py
+++ src/srPolygonTransform.py
@@ -1,109 +1,43 @@
-# ----------------------------------------------------------------------------
-#  SRPolygonTransform (c) by Thomas Mueller
-#
-#    This experimental Blender add-on implements the polygon-rendering 
-#    method for special-relativistic visualization. It transforms a mesh 
-#    object to show what an observer moving with a velocity close to 
-#    the speed of light would see. However, only the geometric distortion is
-#    taken into account and neither the Doppler effect nor any illumination
-#    or shadows will be handled correctly.
-# 
-#    See e.g. "A Survey of Visualization Methods for Special Relativity" 
-#    by Daniel Weiskopf for a brief description:
-#     http://drops.dagstuhl.de/opus/volltexte/2010/2711/pdf/20.pdf
-#
-#    A more detailed explanation can be found in the book
-#    "Spezielle und allgemeine Relativitätstheorie - Grundlagen, Anwendungen 
-#    in Astrophysik und Kosmologie sowie relativistische Visualisierung" 
-#    by Boblest, Mueller, and Wunner; DOI: 10.1007/978-3-662-47767-0
-#
-#
-#  SRPolygonTransform is licensed under a Creative Commons Attribution-
-#  NonCommercial-ShareAlike 4.0 International License.
-#
-#  A copy of the license can be found at 
-#   <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
-# ----------------------------------------------------------------------------
-
-bl_info = {
-    "name" : "SR Polygon Transformation",
-    "author" : "Thomas Mueller",
-    "version" : (0, 0, 1),
-    "blender" : (2, 79, 0),
-    "description" : "Special-relativistic polygon transformation of mesh object.",
-    "category" : "Object"
-}
-
 import bpy
 from mathutils import Vector
 import numpy as np
 from numpy.linalg import inv
-   
 
-# ------------------------------------------------------------
-#  Operator class
-# ------------------------------------------------------------
+
 class ObjectSRPolygonTransform(bpy.types.Operator):
     """
-    Special-relativistic polygon transformation
-    """
+        Special-relativistic polygon transformation
+        """
     bl_idname = "object.sr_polygon_transform"
-    bl_label = "SR Polygon Transformation"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_label = "SR Polygon Transformation Func"
 
-    # observation time
-    t_obs = bpy.props.FloatProperty(
-        name = "tobs", 
-        description = "Observation time",
-        step = 1,
-        default = 0.0)
-
-    # scaled velocity beta_x = v_x / c, where c is the speed of light
-    #beta_x = bpy.props.FloatProperty(
-    #    name = "vx", 
-    #    description = "Scaled velocity in x-direction",
-    #    default = 0.5, 
-    #    step = 1,
-    #    min = -0.99, 
-    #    max = 0.99)
-
-    beta_xyz = bpy.props.FloatVectorProperty(
-        name = "velocity",
-        description = "Scaled velocity",
-        default = (0.5,0.0,0.0),
-        subtype = "DIRECTION",
-        unit='LENGTH')
-
-
-    def kdelta(self,a,b):
+    def kdelta(self, a, b):
         """Kronecker delta"""
         return 1 if a == b else 0
 
-
     def lorentzMatrix(self, beta):
         """
-        Generate Lorentz transformation matrix depending on beta.
-        
-        Args:
-            beta: Scaled velocity beta = v/c where c is the speed of light.
-            
-        Return:
-            Lorentz-Transformation matrix
-        """
+            Generate Lorentz transformation matrix depending on beta.
+
+            Args:
+                beta: Scaled velocity beta = v/c where c is the speed of light.
+
+            Return:
+                Lorentz-Transformation matrix
+            """
         gamma = 1 / np.sqrt(1 - np.dot(beta, beta))
         Lambda = np.identity(4)
-        Lambda[0,0] = gamma
-        for i in range(1,4):
-            Lambda[0,i] = -gamma * beta[i-1]
-            Lambda[i,0] = -gamma * beta[i-1]
-            for j in range(1,4):
-                Lambda[i,j] = self.kdelta(i,j) + gamma**2 / (gamma + 1) * beta[i-1] * beta[j-1]
+        Lambda[0, 0] = gamma
+        for i in range(1, 4):
+            Lambda[0, i] = -gamma * beta[i - 1]
+            Lambda[i, 0] = -gamma * beta[i - 1]
+            for j in range(1, 4):
+                Lambda[i, j] = self.kdelta(i, j) + gamma ** 2 / (gamma + 1) * beta[i - 1] * beta[j - 1]
         return Lambda
-
 
     def evalLT(self, context):
         scene = context.scene
-        
+
         # Active camera within the scene
         cam = scene.camera
 
@@ -116,7 +50,11 @@ class ObjectSRPolygonTransform(bpy.types.Operator):
             return {'FINISHED'}
 
         # Selected object
-        obj = context.selected_objects[0]
+        objraw = context.selected_objects[0]
+
+        obj = objraw.copy()
+
+        bpy.context.collection.objects.link(obj)
 
         if obj.type != 'MESH':
             self.report({'INFO'}, "Selected object has to be a mesh-object!")
@@ -124,7 +62,7 @@ class ObjectSRPolygonTransform(bpy.types.Operator):
 
         # Scaled velocity
         #beta = np.array([self.beta_x,0,0])
-        beta = np.array(self.beta_xyz)
+        beta = np.array(scene.beta_xyz)
 
         # Lorentz transformation matrix for beta
         L = self.lorentzMatrix(beta)
@@ -133,7 +71,7 @@ class ObjectSRPolygonTransform(bpy.types.Operator):
         invL = inv(L)
 
         # Observer is located at origin within its own reference frame
-        obs = np.array([self.t_obs,0,0,0])
+        obs = np.array([scene.t_obs, 0, 0, 0])
 
         # The observer's reference frame has an offset to the global frame
         a1 = np.append([0], cam.location)
@@ -160,26 +98,60 @@ class ObjectSRPolygonTransform(bpy.types.Operator):
 
             # transform emission event into the observer's reference frame
             # here, only the transformation into the global frame is necessary
-            obj1 = invL.dot(obj2) + 0*a2
+            obj1 = invL.dot(obj2) + 0 * a2
             v.co = Vector(obj1[1:])
-
 
     def execute(self, context):
         self.evalLT(context)
 
         return {'FINISHED'}
-    
+
+
+class ObjectSRPolygonTransformPanel(bpy.types.Panel):
+    """
+    Special-relativistic polygon transformation panel
+    """
+    bl_label = "SR Polygon Transform Panel"
+    bl_idname = "OBJECT_PT_sr_polygon_transform"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tool'
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        layout.prop(scene, 't_obs', text="Observation Time")
+        layout.prop(scene, 'beta_xyz', text="Velocity")
+        layout.operator("object.sr_polygon_transform")
+
+
+def updatefunc(self, context):
+    ObjectSRPolygonTransform.execute(self, context)
+
 
 def register():
     bpy.utils.register_class(ObjectSRPolygonTransform)
+    bpy.utils.register_class(ObjectSRPolygonTransformPanel)
     bpy.types.Scene.t_obs = bpy.props.FloatProperty(
-        name = "tobs", 
-        description = "Observation time",
-        default = 0.0)
+        name="Observation Time",
+        default=0.0,
+        step=1,
+        update=updatefunc
+    )
+    bpy.types.Scene.beta_xyz = bpy.props.FloatVectorProperty(
+        name="Velocity",
+        default=(0.5, 0.0, 0.0),
+        subtype="DIRECTION",
+        unit='LENGTH',
+        update=updatefunc
+    )
 
 
 def unregister():
     bpy.utils.unregister_class(ObjectSRPolygonTransform)
+    bpy.utils.unregister_class(ObjectSRPolygonTransformPanel)
+    del bpy.types.Scene.t_obs
+    del bpy.types.Scene.beta_xyz
 
 
 if __name__ == "__main__":
